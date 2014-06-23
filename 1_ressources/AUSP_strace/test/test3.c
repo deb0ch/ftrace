bee10c65932f74ce895db51da2b467ba4b17027b
chauvo_t <deb0ch@bitbucket.org>
2014-06-23 17:19:06 +0200

    import strace into develop

---
+++ 1_ressources/AUSP_strace/test/test3.c
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <err.h>
+
+int	main(int ac, char **av)
+{
+  int	fd;
+
+  write(1, "Hello, World !\n", strlen("Hello, World !\n"));
+  fd = open("./notezfes", O_RDWR);
+  warn("open error %d - ", fd);
+  close(fd);
+  return (EXIT_FAILURE);
+}

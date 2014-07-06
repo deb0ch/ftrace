78a486d19e1dfee84dd62df55cac9229f9f9977f
chauvo_t <deb0ch@bitbucket.org>
2014-07-06 22:02:54 +0200

    norme graph.h

--- include/graph.h
+++ include/graph.h
@@ -1,11 +1,11 @@
 /*
 ** graph.h for ftrace in /home/max/e-wip/ftrace
-** 
+**
 ** Made by bourge_i
 ** Login   <max@epitech.net>
-** 
+**
 ** Started on  Fri Jun 27 17:29:47 2014 bourge_i
-** Last update Sun Jul  6 16:31:10 2014 bourge_i
+** Last update Sun Jul  6 22:02:06 2014 chauvo_t
 */
 
 #ifndef _GRAPH_H_
@@ -13,39 +13,30 @@
 
 # include <stdio.h>
 
-typedef struct          s_graph_node
+typedef struct		s_graph_node
 {
-  int                   id;
-  int                   parent_id;
-  char                  *fct_name;
-  struct s_graph_node   *next;
-}                       t_graph_node;
+  int			id;
+  int			parent_id;
+  char			*fct_name;
+  struct s_graph_node	*next;
+}			t_graph_node;
 
-typedef struct  s_graph
+typedef struct		s_graph
 {
-  t_graph_node          *current;
-  t_graph_node          *graph_list;
-  FILE                  *graph_file;
-  t_graph_node          *(*add_node)(struct s_graph *, char *,
+  t_graph_node		*current;
+  t_graph_node		*graph_list;
+  FILE			*graph_file;
+  t_graph_node		*(*add_node)(struct s_graph *, char *,
                                      t_graph_node *, int);
-  void                  (*pop)(struct s_graph *);
-  int                   (*close_graph)(struct s_graph *);
-}                       t_graph;
+  void			(*pop)(struct s_graph *);
+  int			(*close_graph)(struct s_graph *);
+}			t_graph;
 
-t_graph*                graph_init();
-int                     create_graph(t_graph *);
-int                     print_graph(t_graph *);
-int                     free_graph(t_graph *);
-char                    *find_parent_name(t_graph *, t_graph_node *);
-t_graph_node            *find_parent_node(t_graph *, t_graph_node *);
+t_graph*		graph_init();
+int			create_graph(t_graph *);
+int			print_graph(t_graph *);
+int			free_graph(t_graph *);
+char			*find_parent_name(t_graph *, t_graph_node *);
+t_graph_node		*find_parent_node(t_graph *, t_graph_node *);
 
 #endif /* _GRAPH_H_ */
-
-
-
-
-
-
-
-
-
